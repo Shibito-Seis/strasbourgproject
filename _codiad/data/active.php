a85906e77bc6801c4f38eb92a214c898dea7c31c
Shibito-Seis <44755949+Shibito-Seis@users.noreply.github.com>
2018-11-27 12:53:17 +0100

    Add files via upload

---
+++ _codiad/data/active.php
@@ -0,0 +1,3 @@
+<?php
+/*|["",{"username":"admin","path":"\/app\/license.txt","focused":false},{"username":"admin","path":"\/app\/wp-blog-header.php","focused":false},{"username":"admin","path":"\/app\/wp-activate.php","focused":true}]|*/
+?>
---
+++ _codiad/data/users.php
@@ -0,0 +1,3 @@
+<?php
+/*|[{"username":"Shibito","password":"d6da63bc1b39229b1ee41fc112bc059e0e15523c","project":"\/app"}]|*/
+?>
